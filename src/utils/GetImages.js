b6c8620f0c550b52833f904cfce233cd14316634
MacBook Pro <macbookpro@MacBook-Pro-de-Diego.local>
2022-03-22 19:43:08 -0500

    pre-produccion

--- src/utils/GetImages.js
+++ src/utils/GetImages.js
@@ -1,6 +1,6 @@
 export const getImages=async(name)=>{
     const api="Xj2lLmVKMNS1QQGTiAmKtKvC7RXl2QjB"
-    const URL_BASE="http://api.giphy.com/v1/gifs/search?"
+    const URL_BASE="https://api.giphy.com/v1/gifs/search?"
     const dataFetch=await fetch(`${URL_BASE}api_key=${api}&q=${name}&limit=4`)
     const {data}=await dataFetch.json()
     console.log({data})
